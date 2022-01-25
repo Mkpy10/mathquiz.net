7dd6d7c7ccb74d0ff06ad7f4a0faac0dbec3c292
Mrinank Pandey <79464354+Mkpy10@users.noreply.github.com>
2022-01-25 21:03:00 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+function Login(){
+    localStorage.setItem(docuemnt.getElementById("Username_Input"));
+    localStorage.setItem(document.getElementById("Password_Input"));
+
+    location("index.html")
+}
